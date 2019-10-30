03f8c96e6c64005427b1bf4f1af002cad3320615
Enrique Pineda <enrique@bit-mechanix.com>
2019-10-30 04:43:26 -0400

    shutdown should be a command

--- index.js
+++ index.js
@@ -4,12 +4,10 @@ const program = new commander.Command()
 program.version('0.0.1')
 program
   .option('-d, --debug', 'output extra debugging')
-  .option('-s, --shutdown', 'shutdown')
+
 program.parse(process.argv)
 
 if (!program.debug)
   console.log = () => {}
 else
   console.log(program.opts())
-
-console.log('hi')
